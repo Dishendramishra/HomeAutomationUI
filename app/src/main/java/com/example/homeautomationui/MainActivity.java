fa76fdce9ca4bf35b095e33ba3793239d42d4e59
dishendra <dishendramishra@gmail.com>
2019-04-12 14:09:52 +0530

    added onResume() checkServer feature

--- app/src/main/java/com/example/homeautomationui/MainActivity.java
+++ app/src/main/java/com/example/homeautomationui/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.homeautomationui;
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.Toast;
-import android.os.AsyncTask;
+import android.app.AlertDialog;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     Button socket;
     Button watertank;
     Button chandelier;
+    AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,7 +97,72 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class postRequest extends AsyncTask<String,String,String>{
+    @Override
+    protected void onResume() {
+        super.onResume();
+        alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Connecting to server...");
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+
+        new checkServer().execute();
+
+
+    }
+
+    public class checkServer extends AsyncTask<String, String, String> {
+
+        private int responseCode;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            // Checking if esp8266 server is working
+            ////////////////////////////////////////////////
+            try {
+                URL url = new URL("http://192.168.1.166");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+                responseCode = con.getResponseCode();
+
+                con.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ////////////////////////////////////////////////
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            System.out.println("Request code= "+responseCode);
+
+            if (responseCode == 200)
+                alertDialog.setMessage("Connected !");
+            else
+                alertDialog.setMessage("Couldn't connect to server!");
+
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.setCancelable(true);
+                    alertDialog.show();
+                }
+            });
+        }
+    }
+
+    public class postRequest extends AsyncTask<String, String, String> {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
@@ -102,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 public void run() {
                     Toast toast = Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT);
-                    toast.setGravity(Gravity.TOP|Gravity.END, 0, 0);
+                    toast.setGravity(Gravity.TOP | Gravity.END, 0, 0);
                     toast.show();
                 }
             });
@@ -110,21 +178,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-            try{
+            try {
                 URL url = new URL(strings[0]);
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
                 con.setRequestMethod("GET");
                 con.connect();
-                System.out.println("Status code: "+con.getResponseCode());
+                System.out.println("Status code: " + con.getResponseCode());
                 con.disconnect();
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
 
                 runOnUiThread(new Runnable() {
                     public void run() {
                         Toast toast = Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT);
-                        toast.setGravity(Gravity.TOP|Gravity.END, 0, 0);
+                        toast.setGravity(Gravity.TOP | Gravity.END, 0, 0);
                         toast.show();
                     }
                 });
